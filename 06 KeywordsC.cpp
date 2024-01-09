1f6d534ae36d09c6c96721cc96f85af55d4d1a23
Premanand S <53272042+anandprems@users.noreply.github.com>
2024-01-09 16:17:04 +0530

    Add files via upload

---
+++ 06 KeywordsC.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int sum;
+
+    sum = num1 + num2;
+
+    if (sum > 15) {
+        printf("Sum is greater than 15.\n");
+    } else {
+        printf("Sum is not greater than 15.\n");
+    }
+
+    return 0;
+}
+
